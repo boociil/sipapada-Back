f9995ffe2259a892f846087ae76e2faf949df2ae
boociil <ryanardiansyah208@gmail.com>
2024-11-28 10:17:47 +0700

    adding add ms_keg

--- index.js
+++ index.js
@@ -186,6 +186,7 @@ app.post("/login", async (req,res) => {
 });
 
 
+
 app.post("/add_user",  async (req,res) =>{
     try{
         const { username, password, role,dinas } = req.body;
@@ -289,6 +290,11 @@ app.post('/add_opd', upload.single('file'), (req, res) => {
 });
 
 
+
+app.post("/input_ms_keg", authenticateToken, async (req,res) => {
+    const query = "INSERT INTO `metadata_keg`( `dinas_id`, `tahun`, `nama_kegiatan`, `kode_kegiatan`, `pj_eselon_1`, `pj_eselon_2`, `pj_eselon_3`, `jabatan_es_3`, `alamat_es_3`, `telepon_es_3`, `email_es_3`, `faksimile_es_3`, `cara_pengumpulan`, `sektor_kegiatan`, `rekomendasi`, `id_rekomendasi`, `telepon_intansi`, `email_instansi`, `faksimile`, `latbel_kegiatan`, `tujuan_kegiatan`, `perencanaan_awal`, `perencanaan_akhir`, `desain_awal`, `desain_akhir`, `pengumpulan_awal`, `pengumpulan_akhir`, `pengolahan_awal`, `pengolahan_akhir`, `analisis_awal`, `analisis_akhir`, `diseminasi_awal`, `evaluasi_awal`, `evaluasi_akhir`, `variabel_stat`, `kegiatan_dilakukan`, `jika_berulang`, `tipe_pengumpulan_data`, `cakupan_wilayah`, `wilayah_kegiatan`, `metode_pengumpulan`, `metode_pengumpulan_lainnya`, `sarana_pengumpulan`, `sarana_pengumpulan_lainnya`, `unit_pengumpulan`, `unit_pengumpulan_lainnya`, `rancangan_sampel`, `metode_pemiliahan_sampel_terakir`, `metode_sampel`, `kerangka_sampel_terakir`, `fraksi_sampel`, `sampling_error`, `unit_sampel`, `unit_observasi`, `uji_coba`, `metode_pemeriksaan_kualitas_data`, `metode_pemeriksaan_kualitas_data_lainnya`, `penyesuaian_non_respon`, `petugas_pengumpulan_data`, `persyaratan_pendidikan_terendah_petugas`, `jumlah_petugas`, `jumlah_supervisor`, `jumlah_pengumpul`, `pelatihan_petugas`, `penyuntingan`, `penyandian`, `data_entry`, `penyahihan`, `metode_analisis`, `unit_analisis`, `unit_analisis_lainnya`, `penyajian_analisis`, `penyajian_analisis_lainnya`, `cetak`, `digital`, `data_mikro`) VALUES ('[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]','[value-9]','[value-10]','[value-11]','[value-12]','[value-13]','[value-14]','[value-15]','[value-16]','[value-17]','[value-18]','[value-19]','[value-20]','[value-21]','[value-22]','[value-23]','[value-24]','[value-25]','[value-26]','[value-27]','[value-28]','[value-29]','[value-30]','[value-31]','[value-32]','[value-33]','[value-34]','[value-35]','[value-36]','[value-37]','[value-38]','[value-39]','[value-40]','[value-41]','[value-42]','[value-43]','[value-44]','[value-45]','[value-46]','[value-47]','[value-48]','[value-49]','[value-50]','[value-51]','[value-52]','[value-53]','[value-54]','[value-55]','[value-56]','[value-57]','[value-58]','[value-59]','[value-60]','[value-61]','[value-62]','[value-63]','[value-64]','[value-65]','[value-66]','[value-67]','[value-68]','[value-69]','[value-70]','[value-71]','[value-72]','[value-73]','[value-74]','[value-75]','[value-76]','[value-77]')"
+});
+
 app.post('/login', (req,res) => {
     const {username,password} = req.body;
 
@@ -304,9 +310,6 @@ app.post('/login', (req,res) => {
 
 
 
-app.post("/input_ms_keg", authenticateToken, async (req,res) => {
-
-});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
